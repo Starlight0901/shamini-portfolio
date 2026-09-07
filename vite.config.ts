dfc156fd49940f6a47fb53369a563df4a0fb25e3
Shamini Fernando <126609804+Starlight0901@users.noreply.github.com>
2026-09-07 13:36:25 +0530

    Update portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/shamini-portfolio/',
   build: {
     target: 'es2022',
     rollupOptions: {
